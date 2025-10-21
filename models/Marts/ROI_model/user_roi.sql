c7111aeb0d8169bc4b1ed8b8624d4d8dc0355129
Thomas Jortzig <tom.mot.f52@gmail.com>
2025-10-21 10:29:44 +0000

    created roi table per user

---
+++ models/Marts/ROI_model/user_roi.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table')}}
+
+with
+customer_value as (
+    select * from {{ ref('int_ltv') }}
+),
+
+customer_acquisition_cost as (
+    select * from {{ ref('int_users_with_attribution') }}
+)
+   
+select 
+    ltv.user_id 
+    , ltv.lifetime
+    , ltv.cumulative_daily_revenue
+    , cac.channel 
+    , cac.campaign_id 
+    , cac.attribution_cost
+    , ROUND(DIV0(ltv.cumulative_daily_revenue, cac.attribution_cost), 2) as costumer_roi 
+from customer_value as ltv
+INNER JOIN customer_acquisition_cost as cac
+on ltv.user_id = cac.user_id
